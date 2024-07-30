d94cd4edda19149e31de31d61080ff7c6dcfcbce
João Paulo <joaopptt@hotmail.com>
2024-07-30 20:45:20 -0300

    refactor: change exception filter

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { BadRequestError, NotFoundError } from './errors';
 import { NotFoundErrorFilter } from './not-found-error/not-found-error.filter';
 import { BadRequestErrorFilter } from './bad-request-error/bad-request-error.filter';
+import { PrismaExceptionFilter } from './exception-filters/prisma.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  app.useGlobalFilters(new NotFoundErrorFilter(), new BadRequestErrorFilter());
+  app.useGlobalFilters(new PrismaExceptionFilter());
 
   await app.listen(3000);
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,28 +3,21 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { BadRequestError, NotFoundError } from 'src/errors';
 
 @Injectable()
 export class UsersService {
   constructor(private prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
-    try {
-      const hash = await bcrypt.hash(createUserDto.password, 10);
-      const { password: _, ...props } = createUserDto;
-      const data = {
-        ...props,
-        password: hash,
-      };
-      return await this.prismaService.user.create({
-        data,
-      });
-    } catch (e) {
-      if (e.code === 'P2002') {
-        throw new BadRequestError('User with this email already exists');
-      }
-    }
+    const { password: _password, ...props } = createUserDto;
+
+    const data = {
+      ...props,
+      password: await bcrypt.hash(_password, 10),
+    };
+    return this.prismaService.user.create({
+      data,
+    });
   }
 
   findAll() {
@@ -32,33 +25,29 @@ export class UsersService {
   }
 
   async findOne(id: string) {
-    try {
-      return await this.prismaService.user.findUniqueOrThrow({
-        where: { id },
-      });
-    } catch (e) {
-      if (e.code === 'P2025') {
-        throw new NotFoundError(`User with id ${id} not found`);
-      }
-    }
-  }
-
-  update(id: string, updateUserDto: UpdateUserDto) {
-    return this.prismaService.user.update({
+    return await this.prismaService.user.findUniqueOrThrow({
       where: { id },
-      data: updateUserDto,
     });
   }
 
-  async remove(id: string) {
-    try {
-      return await this.prismaService.user.delete({
-        where: { id },
-      });
-    } catch (e) {
-      if (e.code === 'P2025') {
-        throw new NotFoundError(`User with id ${id} not found`);
-      }
-    }
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const { password: _password, ...props } = updateUserDto;
+    const data = _password
+      ? {
+          ...props,
+          password: await bcrypt.hash(_password, 10),
+        }
+      : updateUserDto;
+
+    return this.prismaService.user.update({
+      where: { id },
+      data,
+    });
+  }
+
+  remove(id: string) {
+    return this.prismaService.user.delete({
+      where: { id },
+    });
   }
 }
