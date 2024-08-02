1ca9621c4df1f0e101c3e704402e5d8e5f394744
João Paulo <joaopptt@hotmail.com>
2024-08-02 11:33:03 -0300

    feat: add User decorator to all credit cards route handler

--- src/credit-cards/credit-cards.controller.ts
+++ src/credit-cards/credit-cards.controller.ts
@@ -10,36 +10,38 @@ import {
 import { CreditCardsService } from './credit-cards.service';
 import { CreateCreditCardDto } from './dto/create-credit-card.dto';
 import { UpdateCreditCardDto } from './dto/update-credit-card.dto';
+import { User } from 'src/auth/decorators/user.decorator';
 
 @Controller('credit-cards')
 export class CreditCardsController {
   constructor(private readonly creditCardsService: CreditCardsService) {}
 
   @Post()
-  create(@Body() createCreditCardDto: CreateCreditCardDto) {
-    return this.creditCardsService.create(createCreditCardDto);
+  create(@Body() createCreditCardDto: CreateCreditCardDto, @User() user) {
+    return this.creditCardsService.create(createCreditCardDto, user.id);
   }
 
   @Get()
-  findAll() {
-    return this.creditCardsService.findAll();
+  findAll(@User() user) {
+    return this.creditCardsService.findAll(user.id);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.creditCardsService.findOne(+id);
+  findOne(@Param('id') id: string, @User() user) {
+    return this.creditCardsService.findOne(+id, user.id);
   }
 
   @Patch(':id')
   update(
     @Param('id') id: string,
     @Body() updateCreditCardDto: UpdateCreditCardDto,
+    @User() user,
   ) {
-    return this.creditCardsService.update(+id, updateCreditCardDto);
+    return this.creditCardsService.update(+id, updateCreditCardDto, user.id);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.creditCardsService.remove(+id);
+  remove(@Param('id') id: string, @User() user) {
+    return this.creditCardsService.remove(+id, user.id);
   }
 }
